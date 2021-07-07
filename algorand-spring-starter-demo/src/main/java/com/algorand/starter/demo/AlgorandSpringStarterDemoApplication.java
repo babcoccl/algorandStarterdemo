6961fe23fad3446780ccbf83c57a14b05a8cb7f5
babcoccl <christianlbabcock@gmail.com>
2021-07-06 22:02:16 -0400

    Initial commit

---
+++ algorand-spring-starter-demo/src/main/java/com/algorand/starter/demo/AlgorandSpringStarterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.algorand.starter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorandSpringStarterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorandSpringStarterDemoApplication.class, args);
+	}
+
+}
