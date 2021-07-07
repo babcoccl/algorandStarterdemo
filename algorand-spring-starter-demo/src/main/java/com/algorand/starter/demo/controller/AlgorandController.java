5f53b43933be862044544c6a515b9902cc176827
babcoccl <christianlbabcock@gmail.com>
2021-07-07 10:45:59 -0400

    Adding some method comments

--- algorand-spring-starter-demo/src/main/java/com/algorand/starter/demo/controller/AlgorandController.java
+++ algorand-spring-starter-demo/src/main/java/com/algorand/starter/demo/controller/AlgorandController.java
@@ -1,7 +1,6 @@
 package com.algorand.starter.demo.controller;
 
 import com.algorand.algosdk.account.Account;
-import com.algorand.algosdk.algod.client.api.AlgodApi;
 import com.algorand.algosdk.v2.client.common.AlgodClient;
 import com.algorand.algosdk.v2.client.common.IndexerClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,32 +21,60 @@ public class AlgorandController {
     @Autowired
     private IndexerClient indexer;
 
-    private AlgodApi api;
     String[] headers = {"X-API-Key"};
     String[] values = {"OVKYBexVj38K5jEMKTwnk45Cg4hVbzeD1RL8zG60"};
 
+    /**
+     * A method to retrieve account details as specified in the properties file
+     * @return String representing account data
+     * @throws Exception
+     */
     @GetMapping(path="/account", produces = "application/json; charset=UTF-8")
     public String account() throws Exception {
         return client.AccountInformation(account.getAddress()).execute(headers, values).body().toString();
     }
 
+    /**
+     * A method to retrieve asset data from the blockchain
+     * @param assetId The asset ID of interest
+     * @return String representing the asset datat
+     * @throws Exception
+     */
     @GetMapping(path="/asset", produces = "application/json; charset=UTF-8")
     public String asset(@RequestParam Long assetId) throws Exception {
         return client.GetAssetByID(assetId).execute(headers, values).body().toString();
     }
 
+    /**
+     * A method to retrieve pending transactions against the account configured in the properties file
+     * @return String representing the pending transactions
+     * @throws Exception
+     */
     @GetMapping(path="/pendingTransactions", produces = "application/json; charset=UTF-8")
     public String pendingTransactions() throws Exception {
         return client.GetPendingTransactionsByAddress(account.getAddress()).execute(headers, values).toString();
     }
 
+    /**
+     * A method to search for an asset by name using the indexer
+     * @param assetName The name of the asset to search for
+     * @return String representing the asset details
+     * @throws Exception
+     */
     @GetMapping(path="/assetSearch", produces = "application/json; charset=UTF-8")
     public String assetSearch(@RequestParam String assetName) throws Exception {
         return indexer.searchForAssets().name(assetName).limit(1L).execute(headers, values).toString();
     }
 
+    /**
+     * A method to search for account transactions before a given date using the indexer
+     * @param date the date of interest to search before
+     * @return String representation of the account transactions
+     * @throws Exception
+     */
     @GetMapping(path = "/accountTransactionsBefore", produces = "application/json; charset=UTF-8")
     public String accountTransactionsBeforeDate(@RequestParam Date date) throws Exception {
-        return indexer.lookupAccountTransactions(account.getAddress()).beforeTime(date).limit(1L).execute(headers, values).toString();
+        return indexer.lookupAccountTransactions(account.getAddress()).beforeTime(date).limit(1L).execute(
+                headers, values).toString();
     }
 }
